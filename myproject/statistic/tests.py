eb52f5e8c2001f7c0122bd21c622f146f980232d
nole23 <nole0223@gmail.com>
2024-08-22 19:33:16 +0200

    test

--- myproject/statistic/tests.py
+++ myproject/statistic/tests.py
@@ -1,49 +1,49 @@
-import os
-import shutil
-from django.test import Client, TestCase
+# import os
+# import shutil
+# from django.test import Client, TestCase
 
-from repository.service import RepositoryService
-from users.models import User
+# from repository.service import RepositoryService
+# from users.models import User
 
-JSON = 'application/json'
+# JSON = 'application/json'
 
-# Create your tests here.
-class TestLogin(TestCase):
-    def setUp(self):
-        self.c = Client()
-        userSave = User.objects.create(firstName="test", lastName="test", email="test@gmail.com", username="test", folderName="test", password="test")
-        user = {
-            'id': userSave.id, 
-            'firstName': userSave.firstName, 
-            'lastName': userSave.lastName, 
-            'username': userSave.username, 
-        }
+# # Create your tests here.
+# class TestLogin(TestCase):
+#     def setUp(self):
+#         self.c = Client()
+#         userSave = User.objects.create(firstName="test", lastName="test", email="test@gmail.com", username="test", folderName="test", password="test")
+#         user = {
+#             'id': userSave.id, 
+#             'firstName': userSave.firstName, 
+#             'lastName': userSave.lastName, 
+#             'username': userSave.username, 
+#         }
 
-        repository = RepositoryService()
-        repositoryData = {
-            'projectName': 'test2', 
-            'type': 'Private', 
-            'descriotion': '', 
-            'typeLanguage': 'Python', 
-            'typeLicense': 'PRIVATE', 
-            'dateOfModifide': '2024-08-15T16:00:59.385Z',
-            'user': user
-        }
-        self.repositorySave = repository.createRepository(repositoryData)
+#         repository = RepositoryService()
+#         repositoryData = {
+#             'projectName': 'test2', 
+#             'type': 'Private', 
+#             'descriotion': '', 
+#             'typeLanguage': 'Python', 
+#             'typeLicense': 'PRIVATE', 
+#             'dateOfModifide': '2024-08-15T16:00:59.385Z',
+#             'user': user
+#         }
+#         self.repositorySave = repository.createRepository(repositoryData)
     
-    def tearDown(self):
-        # Brisanje testnog foldera nakon testa
-        if os.path.exists("media"):
-            shutil.rmtree("media")
+#     def tearDown(self):
+#         # Brisanje testnog foldera nakon testa
+#         if os.path.exists("media"):
+#             shutil.rmtree("media")
 
-    def test_getStatisticByRepositoryId_successifull(self):
-        response = self.c.get('/statistic/' + self.repositorySave['id'], HTTP_AUTHORIZATION='', content_type=JSON)
+#     def test_getStatisticByRepositoryId_successifull(self):
+#         response = self.c.get('/statistic/' + self.repositorySave['id'], HTTP_AUTHORIZATION='', content_type=JSON)
 
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'count_of_ammount')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertContains(response, 'count_of_ammount')
     
-    def test_getStatisticByIncorectyRepositoryId_successifull(self):
-        response = self.c.get('/statistic/1999', HTTP_AUTHORIZATION='', content_type=JSON)
+#     def test_getStatisticByIncorectyRepositoryId_successifull(self):
+#         response = self.c.get('/statistic/1999', HTTP_AUTHORIZATION='', content_type=JSON)
 
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'PROJECT_NOT_FOUND')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertContains(response, 'PROJECT_NOT_FOUND')
