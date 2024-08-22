198e6a67a4633f64267ad30119f7689d7eeece61
nole23 <nole0223@gmail.com>
2024-08-22 19:48:38 +0200

    test

--- myproject/AT/test_users_selenium.py
+++ myproject/AT/test_users_selenium.py
@@ -21,7 +21,7 @@ class UserTests(LiveServerTestCase):
         chrome_options = Options()
         # chrome_options.add_argument("--headless")  # Pokreće Chrome u headless režimu
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        # chrome_options.add_argument("--disable-dev-shm-usage")
         cls.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
         cls.driver.implicitly_wait(10)  # Sačekaj 10 sekundi za elemente
