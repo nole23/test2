bcd48c7b780d0397e603c27fb951075dbf792fc2
nole23 <nole0223@gmail.com>
2024-08-23 08:40:50 +0200

    test 10 docker AT

--- myproject/AT/test_users_selenium.py
+++ myproject/AT/test_users_selenium.py
@@ -47,7 +47,7 @@ class UserTests(LiveServerTestCase):
             current_url  = self.driver.get(f'{self.live_server_url}/')
             print(f"Current URL: {current_url}")  # Print URL nakon što je stranica učitana
 
-             page_source = self.driver.page_source
+            page_source = self.driver.page_source
             print(f"Page source length: {len(page_source)}")  # Print dužinu HTML sadržaja
             print(page_source[:1000])  # Print prvih 1000 karaktera HTML sadržaja
             print("Start 2. 1")
