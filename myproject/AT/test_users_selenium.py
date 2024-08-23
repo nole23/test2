1e7103bfc17b9dc5b52313fab96d622bca24d623
nole23 <nole0223@gmail.com>
2024-08-23 08:26:55 +0200

    Test 8 Docker AT

--- myproject/AT/test_users_selenium.py
+++ myproject/AT/test_users_selenium.py
@@ -20,7 +20,7 @@ class UserTests(LiveServerTestCase):
         super().setUpClass()  # Osiguraj da se osnovne postavke izvrše
 
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
